49d136ee7bcedc16ccf5a6a54391c49cecae2bf0
LiebDenisa <denisalieb@gmail.com>
2025-01-12 14:56:41 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace RecipeManager;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RecipeManager
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
